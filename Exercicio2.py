d1412f6c7161cdb68c966ff751f36ea4ca80dc05
souzaleme <souzaleme@yahoo.com.br>
2022-03-28 17:52:53 -0300

    2-Feito o Exercicio 3

--- Exercicio2.py
+++ Exercicio2.py
@@ -1,4 +1,4 @@
 '''Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]'''
 
-num = input('digite um número: ')
+num = int(input('digite um número: '))
 print(f'o número informado foi {num}')
