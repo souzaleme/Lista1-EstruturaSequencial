1f5f0303bf96e939a0a0621978db16045d622125
souzaleme <souzaleme@yahoo.com.br>
2022-03-28 21:01:13 -0300

    5-Feito o Exercicio 6

---
+++ Exercicio6.py
@@ -0,0 +1,6 @@
+'''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
+
+import math
+raio = float(input(f'Digite o raio do círculo: '))
+area = math.pi * raio ** 2
+print(f'A área do círculo é: {area}')
